0c37b82e638b93e25e581bf7652967c60579fc3b
Aad Versteden <madnificent@gmail.com>
2019-05-21 21:59:44 +0200

    More abstract and defensive rendering of cards
    The rendered cards are more defensive.  They don't render the viewer
    if it is certainly broken.  More information would be needed to
    further enhance the rendering strategy.

--- app/routes/documents/index.js
+++ app/routes/documents/index.js
@@ -3,6 +3,6 @@ import Route from '@ember/routing/route';
 export default class DocumentsIndexRoute extends Route {
 
   async model(){
-    return this.store.query('document', { 'page[size]': 100 });
+    return this.store.query('document', { 'page[size]': 25, include: 'document-versions.file' });
   }
 }
