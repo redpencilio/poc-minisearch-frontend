3e4995ef5f6c0a964962a80269a688926a9be43b
Aad Versteden <madnificent@gmail.com>
2019-05-22 01:00:53 +0200

    Move dynamic fetching of file data into card
    The main logic was already present in the gen-doc service.  We're
    removing the contents now.

--- app/components/document-card.js
+++ app/components/document-card.js
@@ -1,4 +1,40 @@
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
+import fetch from 'fetch';
+
+function asyncFetch( ...args ) {
+  return new Promise( (resolve) => {
+    fetch( ...args ).then( (...response) => resolve( ...response ) );
+  });
+}
 
 export default class DocumentCardComponent extends Component {
+  @tracked
+  showPreview = false
+
+  @tracked
+  htmlSnippet = '';
+
+  @action
+  async togglePreview(){
+    this.showPreview = ! this.showPreview;
+    if( this.showPreview ){
+      this.execute();
+    }
+  }
+
+  @action
+  async execute() {
+    if( this.htmlSnippet != '' ) {
+      if( this.args.documentVersion && this.args.documentVersion.convertedFile )
+      {
+        console.log(`Fetching converted file`);
+        const url = new URL(`/files/${this.args.documentVersion.convertedFile.id}/download`, window.location.href);
+        this.htmlSnippet = await asyncFetch( url );
+      } else {
+        this.htmlSnippet = '';
+      }
+    }
+  }
 }
--- app/routes/documents/index.js
+++ app/routes/documents/index.js
@@ -3,6 +3,6 @@ import Route from '@ember/routing/route';
 export default class DocumentsIndexRoute extends Route {
 
   async model(){
-    return this.store.query('document', { 'page[size]': 25, include: 'document-versions.file' });
+    return this.store.query('document', { 'page[size]': 25, include: 'document-versions.file,document-versions.converted-file' });
   }
 }
